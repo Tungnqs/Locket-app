7170c8e833f28b9639fe9a4636427571586609d7
TungDzaii <tungnqsgch210015@fpt.edu.vn>
2023-12-04 22:23:08 +0700

    delete zoom btn

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-var video = document.getElementById("videoElement");
+var video = document.querySelector("#videoElement");
 
-function initCamera() {
-  navigator.getUserMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia ||
-    navigator.oGetUserMedia;
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia ||
+  navigator.oGetUserMedia;
 
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia({ video: true }, handleVideo, videoError);
-  }
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({ video: true }, handleVideo, videoError);
 }
 
 function handleVideo(stream) {
   video.srcObject = stream;
-  console.log(stream);
   video.play();
 }
 
 function videoError(e) {}
-
-document.getElementById("startCameraButton").addEventListener("click", initCamera);
